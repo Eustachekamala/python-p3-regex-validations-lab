5937f65a44ef8ea8e51b255f23bb4501b7df9467
eustachekamala <eustachejared25@gmail.com>
2024-09-15 23:17:45 +0300

    I added the RegEx of the email and number

--- lib/regex.py
+++ lib/regex.py
@@ -6,31 +6,14 @@ import re
 name = re.compile(r"[A-Z][a-zA-Z-']*(?: [A-Z][a-zA-Z-']*)*")
 name_regex = re.compile(name)
 
-phone_number = r"\(\d{3}\) \d{3}-\d{4}"
+## NOTE: This is a very simple phone number regex. It will not catch all valid phone numbers. 
+##       It is only meant to be used as a starting point for the lab.
+phone_number = r"^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+## NOTE: This is a very simple email regex. It will not catch all valid email addresses. 
+##       It is only meant to be used as a starting point for the lab.
+email_address = r"^[a-zA-Z][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 email_regex = re.compile(email_address)
 
-# # Test function
-# def test_matches_anya_taylor_joy():
-#     '''Matches the string "Anya Taylor-Joy".'''
-#     assert name_regex.fullmatch("Anya Taylor-Joy")
-
-# # Additional test functions
-# def test_matches_phone_number():
-#     '''Matches a phone number in the format (123) 456-7890.'''
-#     assert phone_regex.fullmatch("(123) 456-7890")
-
-# def test_matches_email_address():
-#     '''Matches an email address.'''
-#     assert email_regex.fullmatch("example@example.com")
-    
-
-
-# ## TESTS
-
-# test_matches_anya_taylor_joy()
-# test_matches_phone_number()
-# test_matches_email_address()
 
